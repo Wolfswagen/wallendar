e7f100cf21248cd26207dac25f945e4697cc0bbc
kosea <asy8000@naver.com>
2020-09-23 14:36:27 +0900

    mail

--- src/main/java/com/kosea/wallendar/service/AuthService.java
+++ src/main/java/com/kosea/wallendar/service/AuthService.java
@@ -1,17 +1,22 @@
 package com.kosea.wallendar.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
 @Service
-public class AuthService {
+public class AuthService{
 	@Autowired
 	public JavaMailSender sender;
 
 	public String sendMail(String email) {
+		
 		String code = String.format("%06d", (int) (Math.random() * 10000));
 		SimpleMailMessage simpleMessage = new SimpleMailMessage();
 		simpleMessage.setTo(email);
